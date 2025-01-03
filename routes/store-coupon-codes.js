91d5c7ed3376c4ade02c1b6021ca4efee608250b
Md. Shakib Mia <smdshakibmia2001@gmail.com>
2025-01-03 20:07:26 +0600

    coupon codes functionality added

--- routes/store-coupon-codes.js
+++ routes/store-coupon-codes.js
@@ -23,22 +23,45 @@ router.get("/", verifyJWT, async (req, res) => {
 router.get("/:couponCode", async (req, res) => {
   const { couponCode } = req.params;
   const { couponCodesCollection } = await getCollections();
+  const requestedPlanName = req.headers.planname;
 
-  const couponCodeCursor = await couponCodesCollection.findOne({ couponCode });
-  if (couponCodeCursor !== null) {
-    // const date = new Date().getFullYear()+'-';
-    // console.log(new Date().getMonth());
-    const currentDate = new Date();
-    const validFrom = new Date(couponCodeCursor.validFrom);
-    const validTill = new Date(couponCodeCursor.validTill);
+  try {
+    // Find the coupon code in the collection
+    const couponCodeCursor = await couponCodesCollection.findOne({
+      couponCode,
+    });
 
-    if (currentDate >= validFrom && currentDate <= validTill) {
-      res.send(couponCodeCursor);
+    if (couponCodeCursor !== null) {
+      const currentDate = new Date();
+      const validFrom = new Date(couponCodeCursor.validFrom);
+      const validTill = new Date(couponCodeCursor.validTill);
+
+      // Check if the plan is associated with the coupon
+      if (!couponCodeCursor.plan.includes(requestedPlanName)) {
+        return res.status(400).send({
+          message: "Invalid Coupon Code for this Plan",
+        });
+      }
+
+      // Check if the coupon is within the valid date range
+      if (currentDate >= validFrom && currentDate <= validTill) {
+        return res.send(couponCodeCursor);
+      } else {
+        return res.status(410).send({
+          message: "Coupon Code Expired",
+        });
+      }
     } else {
-      res.status(410).send({ message: "Coupon Code Expired" });
+      // Coupon code is invalid
+      return res.status(400).send({
+        message: "Invalid Coupon Code",
+      });
     }
-  } else {
-    res.status(400).send({ message: "Invalid Coupon Code" });
+  } catch (error) {
+    console.error("Error processing coupon code:", error);
+    return res.status(500).send({
+      message: "Internal Server Error",
+    });
   }
 });
 
